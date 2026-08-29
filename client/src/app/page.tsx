577321ca9460d28be456003155849c71c836a303
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-08-30 02:10:48 +0700

    8. Sử dụng Image trong Next.js (với next/image)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <h1 className="font-normal text-4xl text-center">
-        Xin chào mọi người, Được Dev đây!
-      </h1>
+      <div className="w-[700px] h-[700px] bg-red-300">
+        {/* <img
+          src="/images/suffer.png"
+          className="w-[500px] h-[500px]"
+          alt="suffer"
+        /> */}
+        <Image
+          src="https://images.pexels.com/photos/19400187/pexels-photo-19400187/free-photo-of-cat-sa-m-c-xe-h-i-du-l-ch.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="pexels"
+          width={600}
+          height={400}
+          className="w-[500px] h-[500px]"
+          priority
+        />
+      </div>
     </>
   );
 }
