d0172dcec5c20fc7c2fac19dedb28bc49574b726
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2026-08-31 01:01:09 +0700

    12. Server Component

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,14 +1,22 @@
-"use client";
-import { useState } from "react";
+// "use client";
+// import { useState } from "react";
+import { websites } from "@/lib/data";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("duthanhduoc@gmail.com");
-  console.log("LoginPage");
+  // const [email, setEmail] = useState("duthanhduoc@gmail.com");
 
   return (
     <>
       <div>Login Page</div>
-      {email}
+      <ul>
+        {websites.map((item) => (
+          <li key={item.url}>
+            <a href={item.url} target="_blank">
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
